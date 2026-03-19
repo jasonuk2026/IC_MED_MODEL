85092029b8dd5f25c73c00365a759e2208f29075
Jason <z.duan@imperial.ac.uk>
2026-03-19 00:46:49 +0000

    HPC

--- run_all_tasks.sh
+++ run_all_tasks.sh
@@ -4,32 +4,34 @@ set -e
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 TASKS=(
-    new_hypertension
+    # new_hypertension
     # new_hyperlipidemia
     # new_pancan
-    # new_celiac
+    new_celiac
     # new_lupus
     # new_acutemi
 )
 
 for TASK in "${TASKS[@]}"; do
     echo "========== $TASK  [val — threshold search] =========="
-    torchrun --nproc_per_node=4 predict_logits.py \
-        data/llm_inputs/new_diagnosis/${TASK}_all.parquet \
+    conda run -n torch torchrun --nproc_per_node=4 predict_logits.py \
+        $PROJECTDIR/zduan/data/ehrshot_extracted/new_diagnosis/${TASK}_all.parquet \
         --split val \
+	--model Qwen/Qwen3.5-122B-A10B \
         --batch_size 4 \
         --num_workers 4 \
         --max_event_tokens 30000 \
-        --output_dir results_tp_reproduce \
+        --output_dir results_tp \
         --tensor_parallel
 
     echo "========== $TASK  [test — evaluation] =========="
-    torchrun --nproc_per_node=4 predict_logits.py \
-        data/llm_inputs/new_diagnosis/${TASK}_all.parquet \
+    conda run -n torch torchrun --nproc_per_node=4 predict_logits.py \
+        $PROJECTDIR/zduan/data/ehrshot_extracted/new_diagnosis/${TASK}_all.parquet \
         --split test \
+	--model Qwen/Qwen3.5-122B-A10B \
         --batch_size 4 \
         --num_workers 4 \
         --max_event_tokens 30000 \
-        --output_dir results_tp_reproduce \
+        --output_dir results_tp \
         --tensor_parallel
 done
